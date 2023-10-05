3f86b0730afbc20b0f9539da892ea431a08bfecc
EricNorrwing <Eric.Norrwing@gmail.com>
2023-10-05 09:35:53 +0200

    first commit

---
+++ src/com/EricNorrwing/Lektion_8_demo/Main.java
@@ -0,0 +1,16 @@
+package com.EricNorrwing.Lektion_8_demo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Annotationer
+        //Manuellt eller automagiskt
+        //Säkrare kod
+
+
+
+
+
+
+    }
+}
